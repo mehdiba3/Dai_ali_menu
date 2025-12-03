8e04f90f69e752ebc8ae67f7fd734ec738e2eae9
mehdiba3 <mehdibakhodazadeh@gmail.com>
2025-12-03 10:54:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// آکاردئون باز/بسته کردن دسته‌ها
 const acc = document.getElementsByClassName('accordion-btn');
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function () {
     this.classList.toggle('active');
     const panel = this.nextElementSibling;
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
+    if (panel.style.display === 'block') { panel.style.display = 'none'; }
+    else { panel.style.display = 'block'; }
   });
 }
